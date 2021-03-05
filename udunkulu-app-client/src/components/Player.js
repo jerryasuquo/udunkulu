b6c8a23db2c7b286eac8ab49edb19a90051fa744
MircleUFO <miracleufodiama@gmail.com>
2021-03-05 04:16:54 +0100

    Player works perfectly except when routes change

--- udunkulu-app-client/src/components/Player.js
+++ udunkulu-app-client/src/components/Player.js
@@ -1,12 +1,13 @@
 import React from "react";
 import back from '../assets/img/back.png';
 import play from '../assets/img/play.png';
+import mute from '../assets/img/mute.png';
 import pause from '../assets/img/pause.png';
 import forward from '../assets/img/forward.png';
 import shuffle from '../assets/img/shuffle.png';
 import shuffleOrng from '../assets/img/shuffle-orange.png';
-import albumArt from '../assets/img/album-art.png';
 import favorites from '../assets/img/favorites.png';
+import favoritesOrng from '../assets/img/favorites-orange.png';
 import loop from '../assets/img/loop.png';
 import loopOrng from '../assets/img/loop-orange.png';
 import volume from '../assets/img/volume.png';
@@ -15,10 +16,20 @@ import '../assets/css/Player.css';
 class Player extends React.Component {
 
   state = {
+  src: this.props.src,
+  deetsList: this.props.deetsList,
+  index: this.props.index,
   playing: this.props.playing,
   title: this.props.title,
   artist: this.props.artist,
-  loop: false,
+  cover: this.props.cover,
+  releaseDate: this.props.releaseDate,
+  album: this.props.album,
+  muted: false,
+  volume: 0.5,
+  duration: 0,
+  currentTime: 0,
+  loop: 0,
   shuffle: false
   }
 
@@ -26,7 +37,11 @@ class Player extends React.Component {
     if (prevProps.x != this.props.x) {
       let audio = document.getElementById('player');
       audio.play();
-      this.setState({...this.state, playing: true});
+      this.setState({
+        ...this.state, playing: true, index: this.props.index, src: this.props.src,
+        artist: this.props.artist, title: this.props.title, cover: this.props.cover,
+        album: this.props.album, releaseDate: this.props.releaseDate, deetsList: this.props.deetsList
+      });
     }
   }
 
@@ -42,49 +57,166 @@ class Player extends React.Component {
   }
 
   handleToggleNext = (e) => {
-    document.getElementById('player').pause();
-    let src = e.target.getAttribute('id') == 'back' ? this.props.prevSrc : this.props.nextSrc;
-    document.getElementById('player').setAttribute('src', src);
-    document.getElementById('player').play();
+    let audio = document.getElementById('player');
 
-    let title = e.target.getAttribute('id') == 'back' ? this.props.prevTitle : this.props.nextTitle;
-    let artist = e.target.getAttribute('id') == 'back' ? this.props.prevArtist : this.props.nextArtist;
-    this.setState({...this.state, playing: true, title: title, artist: artist});
+    //Sets index of current song navigated to in state
+    if(e.target.getAttribute('id') == 'back') {
+      this.props.deetsList[this.state.index - 1] ? this.state.index-- : this.state.index = this.props.deetsList.length - 1; 
+    } else {
+      this.props.deetsList[this.state.index + 1] ? this.state.index++ : this.state.index = 0; 
+      }     
+    
+    //Sets new src to play current song
+    let src = this.props.deetsList[this.state.index].src;
+    audio.pause();
+    audio.setAttribute('src', src);
+    audio.play();
+
+    //Sets current songs deets
+    let title = this.props.deetsList[this.state.index].title;
+    let artist = this.props.deetsList[this.state.index].artist;
+    let cover = this.props.deetsList[this.state.index].cover;
+    let releaseDate = this.props.deetsList[this.state.index].releaseDate;
+    let album = this.props.deetsList[this.state.index].album;
+    this.setState({...this.state, playing: true, title: title, artist: artist, cover: cover, releaseDate: releaseDate, album: album});
   }
+
+
+  handleEnded = (e) => {
+    let audio = document.getElementById('player');
+
+    //Sets index to random song's index if in shuffle and not in loop
+    if(this.state.shuffle && this.state.loop !== 2) {
+      this.state.index = Math.floor(Math.random() * (this.props.deetsList.length - 1));
+    }
+
+    if (this.state.loop == 2) {
+      audio.setAttribute('loop', true);
+    }
+
+    //Sets index to next song if not in song loop
+    if (this.state.loop == 1) {
+      this.props.deetsList[this.state.index + 1] ? this.state.index++ : this.state.index = 0;
+    }
+
+    //Sets song to next song if not in playlist loop and stops playlist at last song
+    if (this.state.loop == 0) {
+      if (this.props.deetsList[this.state.index + 1]) {
+        this.state.index++;
+      } else {
+        audio.pause();
+        this.setState({...this.state, playing: false})
+      }
+    }
+
+    if (this.props.deetsList[this.state.index + 1]) {
+      let src = this.props.deetsList[this.state.index].src;
+      audio.setAttribute('src', src);
+      let title = this.props.deetsList[this.state.index].title;
+      let artist = this.props.deetsList[this.state.index].artist;
+      let cover = this.props.deetsList[this.state.index].cover;
+      let releaseDate = this.props.deetsList[this.state.index].releaseDate;
+      let album = this.props.deetsList[this.state.index].album;
+      this.setState({...this.state, playing: true, title: title, artist: artist, cover: cover, releaseDate: releaseDate, album: album});
+    } 
+  }
+
 
   handleClickLoop = (e) => {
-    document.getElementById('player').setAttribute('loop', true);
-    !this.state.loop ? this.setState({...this.state, loop: true}) : this.setState({...this.state, loop: false});;
+    if (this.state.loop == 0) {
+      this.setState({...this.state, loop: 1});
+    } 
+
+    if(this.state.loop == 1) {
+      this.setState({...this.state, loop: 2, shuffle: false});
+      document.getElementById('player').setAttribute('loop', true);
+    }
+
+    if(this.state.loop == 2) {
+      this.setState({...this.state, loop: 0});
+      document.getElementById('player').setAttribute('loop', false);
+    }
   }
 
+
   handleClickShuffle = (e) => {
-    !this.state.shuffle ? this.setState({...this.state, shuffle: true}) : this.setState({...this.state, shuffle: false});
+    !this.state.shuffle ? 
+    this.state.loop == 2 ? this.setState({...this.state, shuffle: true, loop: 0}) : this.setState({...this.state, shuffle: true, loop: this.state.loop})
+    : this.setState({...this.state, shuffle: false});
+  }
+
+
+  handleClickFavorite = (e) => {
+    !this.state.favorites ? this.setState({...this.state, favorites: true}) : this.setState({...this.state, favorites: false});
+  }
+
+  handleTimelineChange = (e) => {
+    document.getElementById('player').currentTime = e.target.value;
+  }
+
+  handleTimeUpdate = (e) => {
+    let duration = Math.ceil(e.target.duration);
+    let currentTime = e.target.currentTime;
+    this.setState({...this.state, currentTime: currentTime, duration: duration});
+  }
+
+  handleVolumeChange = (e) => {
+    let volume = e.target.value;
+    this.setState({...this.state, volume: volume});
+    document.getElementById('player').volume = this.state.volume;
+  }
+
+  handleToggleMute = (e) => {
+    !this.state.muted ? this.setState({...this.state, muted: true}) : this.setState({...this.state, muted: false});
   }
 
   render() {
     return (
       <footer>
         <section>
-          <audio src={this.props.src} id='player' autoPlay>
+          <audio src={this.state.src} id='player' autoPlay muted={this.state.muted} preload='auto' onEnded={this.handleEnded} onPlay={this.handlePlay} onTimeUpdate={this.handleTimeUpdate}>
           </audio>
   
           <div id='control-panel'>
+            <input type='range' id='timeline' min='0' max={`${this.state.duration}`} value={this.state.currentTime} onChange={this.handleTimelineChange} />
             <div>
               <img src={back} id='back' alt='Click to play previous' onClick={this.handleToggleNext} />
-              {!this.state.playing ? <img src={play} alt='Click to play' onClick={this.handleTogglePlay}/> : <img src={pause} alt='Click to pause' onClick={this.handleTogglePlay}/>}
+              {!this.state.playing ? 
+                <img src={play} alt='Click to play' onClick={this.handleTogglePlay} /> 
+              : <img src={pause} alt='Click to pause' onClick={this.handleTogglePlay} />
+              }
               <img id='forward' src={forward} alt='Click to play next' onClick={this.handleToggleNext} />
             </div>
             
-            <div>
-              <span></span>
-              <div id='albumInfo'><img src={albumArt} alt='Album art' id='albumArt' /><div><h1>{this.state.title}</h1><p>{this.state.artist}</p></div></div>
-              <ul><li>ABT</li><li>2017</li></ul>
+            <div id='albumInfo-container'>
+              <div id='albumInfo'><img src={this.state.cover} alt='Album art' id='albumArt' /><div><h1>{this.state.title}</h1><p>{this.state.artist}</p></div></div>
+              <ul><li>{this.state.album}</li><li>{this.state.releaseDate}</li></ul>
             </div>
   
             <div>
-              <img src={favorites} />
-              <img src={volume} />
-              {!this.state.loop ? <img src={loop} onClick={this.handleClickLoop} alt='Turn on loop' /> : <img src={loopOrng} onClick={this.handleClickLoop} alt='Turn off loop' />}
+
+              {!this.state.favorites ?
+                <img src={favorites} onClick={this.handleClickFavorite} alt='Add to favorites' /> 
+              : <img src={favoritesOrng} onClick={this.handleClickFavorite} alt='Remove from favorites' />
+              }
+
+              <div id='volume-container'>
+                <input type='range' id='volume-range' min='0' max='1' step='0.1' value={`${this.state.volume}`} onChange={this.handleVolumeChange} />
+                {!this.state.muted ?
+                  <img src={volume} onClick={this.handleToggleMute} />
+                :
+                  <img src={mute} onClick={this.handleToggleMute} />
+                }
+              </div>
+          
+              {this.state.loop == 0 ?
+                <img src={loop} onClick={this.handleClickLoop} alt='Turn on loop' /> 
+              : <div>
+                  <img src={loopOrng} onClick={this.handleClickLoop} alt='Turn off loop' />
+                  {this.state.loop == 2 ? <span id='loop-superscript'>1</span> : ''}
+                </div>
+              }
+
               {!this.state.shuffle? <img src={shuffle} onClick={this.handleClickShuffle} alt='Turn on shuffle' /> : <img src={shuffleOrng} onClick={this.handleClickShuffle} alt='Turn on shuffle' />}
             </div>
           </div>
--- udunkulu-app-client/src/components/Playlist.js
+++ udunkulu-app-client/src/components/Playlist.js
@@ -10,34 +10,56 @@ let Playlist = () => {
 
   const [state, setState] = useState({
     src: '',
-    prevSrc: '',
-    nextSrc: '',
     title: '',
-    prevTitle: '',
-    nextTitle: '',
     artist: '',
-    prevArtist: '',
-    nextArtist: '',
+
+    deetsList: [
+      {title: 'FEM', src: '/audio/ABetterTime/FEM.mp3', artist: 'Davido', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Jowo', src: '/audio/ABetterTime/Jowo.mp3', artist: 'Davido', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Something Fishy', src: '/audio/ABetterTime/SomethingFishy.mp3', artist: 'Davido', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Holy Ground', src: '/audio/ABetterTime/HolyGround.mp3', artist: 'Davido, Nicki Minaj', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Heaven', src: '/audio/ABetterTime/Heaven.mp3', artist: 'Davido', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Very Special', src: '/audio/ABetterTime/VerySpecial.mp3', artist: 'Davido', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'The Best', src: '/audio/ABetterTime/TheBest.mp3', artist: 'Davido, Mayorkun', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Shopping Spree', src: '/audio/ABetterTime/ShoppingSpree.mp3', artist: 'Davido, Chris Brown, Young Thug', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Sunlight', src: '/audio/ABetterTime/Sunlight.mp3', artist: 'Davido', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Tanana', src: '/audio/ABetterTime/Tanana.mp3', artist: 'Davido, Tiwa Savage', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Mebe', src: '/audio/ABetterTime/Mebe.mp3', artist: 'Davido, Mugeez', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'La La', src: '/audio/ABetterTime/LaLa.mp3', artist: 'Davido, CKay', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'So Crazy', src: '/audio/ABetterTime/SoCrazy.mp3', artist: 'Davido, Lil Baby', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Birthday Cake', src: '/audio/ABetterTime/BirthdayCake.mp3', artist: 'Davido, Nas, Hit-Boy', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'I Got a Friend', src: '/audio/ABetterTime/IGotAFriend.mp3', artist: 'Davido, Mayorkun, Sho Madjozi', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'Fade', src: '/audio/ABetterTime/Fade.mp3', artist: 'Davido, Bella Shmurda', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+      {title: 'On My Way', src: '/audio/ABetterTime/OnMyWay.mp3', artist: 'Davido, Sauti Sol', cover: '/audio/ABetterTime/fem-album-art.png', album: 'ABT', releaseDate: '2017'},
+    ],
+
     playing: false,
+    index: 0,
     x: 0
   });
 
   let handleClick = (e) => {
+    let deetsList = state.deetsList;
+    let dataIndex = e.target.getAttribute('data-index');
+    dataIndex = Number(dataIndex);
+    let src = deetsList[dataIndex].src;
 
-    let src = e.target.getAttribute('data-src');
-    let title = e.target.getAttribute('data-title');
-    let artist = e.target.getAttribute('data-artist');
+    let title = deetsList[dataIndex].title;
 
+    let artist = deetsList[dataIndex].artist;
+
+    let cover = deetsList[dataIndex].cover;
+
+    let album = deetsList[dataIndex].album;
+
+    let releaseDate = deetsList[dataIndex].releaseDate;
 
     setState({
-      ...state, 
-      src: src, prevSrc: prevSrc, nextSrc: nextSrc,  
-      title: title, prevTitle: prevTitle, nextTitle: nextTitle,
-      artist: artist, prevArtist: prevArtist, nextArtist: nextArtist,
-      playing: true, x: state.x + 1
+      ...state, src: src, cover: cover, title: title,
+      artist: artist, playing: true, x: state.x + 1,
+      index: dataIndex, album: album, releaseDate: releaseDate
     });
-
-    e.stopPropagation();
+    document.getElementById('list').setAttribute('class', 'list-shorter');
   }
 
  return (
@@ -45,25 +67,27 @@ let Playlist = () => {
       <LibraryNav />
       <PlaylistMenu />
       <div id='playlist'>
-        
         <section className='list-container'>
         <section className='list'>
         <h1>Now Playing</h1>
-        <div>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>01</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/Jowo.mp3' data-title='Jowo' data-artist='Davido' onClick={handleClick}><span>02</span>Jowo</p><p>3:25</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>03</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/Jowo.mp3' data-title='Jowo' data-artist='Davido' onClick={handleClick}><span>04</span>Jowo</p><p>3:25</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>05</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>06</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>07</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>08</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>09</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>10</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>11</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>12</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>13</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
-          <li><div className='flexBox'><p data-src='/audio/ABetterTime/FEM.mp3' data-title='FEM' data-artist='Davido' onClick={handleClick}><span>14</span>FEM</p><p>3:22</p></div><img src={line} alt=''/></li>
+        <div id='list' className='list-longer'>
+          <li><div className='flexBox' data-index={0} onClick={handleClick}><p data-index={0}><span>01</span>FEM</p><p data-index={0}>3:22</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={1} onClick={handleClick}><p data-index={1}><span>02</span>Jowo</p><p data-index={1}>2:56</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={2} onClick={handleClick}><p data-index={2}><span>03</span>Something Fishy</p><p data-index={2}>2:53</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={3} onClick={handleClick}><p data-index={3}><span>04</span>Holy Ground</p><p data-index={3}>2:42</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={4} onClick={handleClick}><p data-index={4}><span>05</span>Heaven</p><p data-index={4}>2:44</p></div><img src={line} alt=''/></li>
+          <li ><div className='flexBox' data-index={5} onClick={handleClick}><p data-index={5}><span>06</span>Very Special</p><p data-index={5}>3:10</p></div><img src={line} alt=''/></li>
+          <li ><div className='flexBox' data-index={6} onClick={handleClick}><p data-index={6}><span>07</span>The Best</p><p data-index={6}>2:44</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={7} onClick={handleClick}><p data-index={7}><span>08</span>Shopping Spree</p><p data-index={7}>2:51</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={8} onClick={handleClick}><p data-index={8}><span>09</span>Sunlight</p><p data-index={8}>3:33</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={9} onClick={handleClick}><p data-index={9}><span>10</span>Tanana</p><p data-index={9}>3:42</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={10} onClick={handleClick}><p data-index={10}><span>11</span>Mebe</p><p data-index={10}>4:00</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={11} onClick={handleClick}><p data-index={11}><span>12</span>La La</p><p data-index={11}>3:07</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={12} onClick={handleClick}><p data-index={12}><span>13</span>So Crazy</p><p data-index={12}>2:50</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={13} onClick={handleClick}><p data-index={13}><span>14</span>Birthday Cake</p><p data-index={13}>2:50</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={14} onClick={handleClick}><p data-index={14}><span>15</span>I Got a Friend</p><p data-index={14}>4:38</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={15} onClick={handleClick}><p data-index={15}><span>16</span>Fade</p><p data-index={15}>3:26</p></div><img src={line} alt=''/></li>
+          <li><div className='flexBox' data-index={16} onClick={handleClick}><p data-index={16}><span>17</span>On My Way</p><p data-index={16}>3:50</p></div><img src={line} alt=''/></li>
         </div>
         </section>
         </section>
@@ -79,13 +103,15 @@ let Playlist = () => {
         </section>
       </div>
 
-      { state.playing ? 
-      <Player src={state.src} prevSrc={state.prevSrc} nextSrc={state.nextSrc} 
-      title={state.title} prevTitle={state.prevTitle} nextTitle={state.nextTitle}
-      artist={state.artist} prevArtist={state.prevArtist} nextArtist={state.nextArtist}
-      playing={state.playing} x={state.x} /> 
-       :  
-       '' }
+      {state.playing ? 
+      <Player 
+        deetsList={state.deetsList} playing={state.playing} 
+        cover={state.cover} title={state.title} artist={state.artist}
+        x={state.x} index={state.index} src={state.src} 
+        album={state.album} album={state.album} releaseDate={state.releaseDate}
+      /> 
+      :  
+      ''}
     </div>
   );
 }
